6516b977b08e24919654f93fcb73ebfcc9283aa7
D.Biles <darron.biles@gmail.com>
2018-11-14 00:59:50 -0500

    middlware, home page, and server set

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = (app) => {
+    // Route for the survey page
+    app.get(`/survey`, (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+
+    // Route for the main / home page
+    app.use('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+};
--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/app/public')));
+
+// Start Server
 app.listen(PORT, ()=> {
   console.log(`Friender is now started and listening on port ${PORT}`);
   console.log(path.join(__filename));
-})
+});
+
+// Routes
+require('./app/routing/htmlRoutes')(app);
