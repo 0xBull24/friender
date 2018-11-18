ed67bf71b722d3b95e58de6f7762489665ed4d3d
D.Biles <darron.biles@gmail.com>
2018-11-17 20:45:13 -0500

    small update to the files. Collecting survey data

--- app/data/listener.js
+++ app/data/listener.js
@@ -1,24 +1,3 @@
 $(function() {
-    // Submit Button
-    $('.submit').on('click', function(event) {
-        event.preventDefault();
-        console.log('Clicking the submit');
-
-        // Set variables
-        first = $('#first-name').val().trim() || "";
-        last = $('#last-name').val().trim() || "";
-        
-        let formResult = {
-            name: `${first} ${last}`,
-            img: `${$('#user-image').val().trim()}`,
-            results: [],
-        }
-
-        for (let count = 1; count < 11; count++) {
-            formResult.results.push($(`#q${count}`).val());
-        }
-
-        // Clear values of the form
-        $('#form').trigger('reset');
-    });
+    
 })
